8ec4a41754b19b0ffbaa08fc87d7ba7a57828f12
Cat1tac <waynepowell235@gmail.com>
2025-10-12 20:47:28 -0400

    signup controller added

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, res) => {
+    //Create Token for user and check if they are authenticated
+    const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: "7"});
+
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, //7 days in MS
+        httpOnly: true, //prevent XSS attacks: cross-stite scripting
+        sameSite: "strict", //CSRF attacks
+        secure: process.env.NODE_ENV === "development" ? false : true 
+    });
+    return token;
+}
