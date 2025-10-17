d68ad5791bd6fbdb21ce638a34425d29304ccbc2
Cat1tac <waynepowell235@gmail.com>
2025-10-17 16:35:32 -0400

    Created welcome email functionality

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
     //Check if JWT_SECRET exists
-    const {JWT_SECRET} = process.env;
+    const {JWT_SECRET} = ENV;
     if (!JWT_SECRET){
         throw new Error("JWT_SECRET is no configured");
     }
@@ -14,7 +15,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, //7 days in MS
         httpOnly: true, //prevent XSS attacks: cross-stite scripting
         sameSite: "strict", //CSRF attacks
-        secure: process.env.NODE_ENV === "development" ? false : true 
+        secure: ENV.NODE_ENV === "development" ? false : true 
     });
     return token;
 }
